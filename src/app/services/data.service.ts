8b92eee8e5251cc318dd82c53553adc944b1b2f8
Shahar Alaluf <alaluf99@gmail.com>
2019-12-26 02:13:00 +0200

    first

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {RoomStatusEnum} from "../entities/room";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  SERVER_URL = 'http://192.168.43.218:8080';
 
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
+
+  addRoom() {
+    this.httpClient.post('/rooms', {name: 'חדר מלך', status: RoomStatusEnum.WAITING, cards: [], users: []});
+  }
 }

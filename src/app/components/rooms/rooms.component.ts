e6351f90d07e25fc5d8de77088e3e6a2912ee1f4
Shahar Alaluf <alaluf99@gmail.com>
2019-12-26 02:01:26 +0200

    first

--- src/app/components/rooms/rooms.component.ts
+++ src/app/components/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Room, RoomStatusEnum} from "../../entities/room";
 
 @Component({
   selector: 'app-rooms',
@@ -6,10 +7,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rooms.component.less']
 })
 export class RoomsComponent implements OnInit {
+  columnsToDisplay = ['name', 'numberOfPlayers', 'status', 'position'];
+  dataSource;
+  columnsInHebrew = {
+    name: 'שם חדר',
+    status: 'סטטוס',
+    numberOfPlayers: 'מספר משתתפים'
+  };
+
+  ELEMENT_DATA: Room []  = [
+    {
+      name: 'חדר מלך',
+      numberOfPlayers: 4,
+      status: RoomStatusEnum.STARTED
+    },
+    {
+      name: 'חדר מלך',
+      numberOfPlayers: 4,
+      status: RoomStatusEnum.STARTED
+    },
+    {
+      name: 'חדר מלך',
+      numberOfPlayers: 4,
+      status: RoomStatusEnum.STARTED
+    },
+    {
+      name: 'חדר מלך',
+      numberOfPlayers: 4,
+      status: RoomStatusEnum.STARTED
+    },
+    {
+      name: 'חדר מלך',
+      numberOfPlayers: 4,
+      status: RoomStatusEnum.STARTED
+    },
+    {
+      name: 'חדר מלך',
+      numberOfPlayers: 4,
+      status: RoomStatusEnum.STARTED
+    }
+  ]
 
   constructor() { }
 
   ngOnInit() {
+    this.dataSource = this.ELEMENT_DATA;
   }
 
+  enterRoom() {
+
+  }
 }
